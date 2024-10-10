9345614c821727aad69eb75599e88361f5de141f
maku25 <kut.kemal@icloud.com>
2024-10-10 17:01:20 +0200

    crud

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Thing = require('./models/Thing');
+
 require('dotenv').config({ path: '../.env' });
 
 const app = express();
@@ -17,52 +19,37 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({ 
-        message: 'Objet créé !' 
-    });
+app.post('/api/stuff', (req, res, next) => { //création d'1 obj
+    delete req.body._id;
+    const thing = new Thing({ ...req.body });
+    thing.save()
+        .then(() => res.status(201).json('objet enregistré !'))
+        .catch(error => res.status(400).json({ error }));
 });
 
-app.use('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+
+app.get('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }));
 });
 
-app.use((req, res, next) => {
-    console.log("requête reçue");
-    next();
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+        .catch(error => res.status(400).json({ error }));
 });
 
-app.use((req, res, next) => {
-    res.json({ message: 'requete okk'});
-    next();
-});
-
-app.use((req, res) => {
-    console.log("réponse envoyée avec succès !");
+app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
