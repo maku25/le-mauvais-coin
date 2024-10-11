d209f084c587a8560e568b6d7089c7b0eb63a5a5
maku25 <kut.kemal@icloud.com>
2024-10-11 11:34:21 +0200

    multer : fichiers

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', auth, stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/', auth, stuffCtrl.getAllThings);
 router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
