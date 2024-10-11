f892ff1eba423b0bba977ab0891e50c2ae42fdc5
maku25 <kut.kemal@icloud.com>
2024-10-11 10:47:09 +0200

    middleware authentification

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const auth = require('../middleware/auth');
+
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', stuffCtrl.createThing);
-router.get('/', stuffCtrl.getAllThings);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.post('/', auth, stuffCtrl.createThing);
+router.get('/', auth, stuffCtrl.getAllThings);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
